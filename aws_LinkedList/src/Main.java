d1fa5ed6115524092b5a2ecb3817ecaed1128044
satyam-kalihari <satyamkalihari17@gmail.com>
2025-05-23 18:48:25 +0530

    Linked List

---
+++ aws_LinkedList/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> clues = new LinkedList<>();
+
+        clues.add("Check inside the mail box");
+        clues.add("Go to the fountain in the park");
+        clues.add("Look for the Oak tree");
+
+        System.out.println(clues);
+    }
+}
